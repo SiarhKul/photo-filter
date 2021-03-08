b5c06d52bbf27742e910a9cd45eef4a392cce51e
SiarhKul <SiarhKul@gmail.com>
2021-03-08 14:32:55 +0300

    fix:fix staturation value

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+const btns = document.querySelectorAll('.btn')
+const inputs = document.querySelectorAll('.filters input')
+const setProps = document.documentElement.style
 
 document.querySelector('.fullscreen').addEventListener('click', () => {
   if (!document.fullscreenElement) {
@@ -14,26 +16,40 @@ document.querySelector('.fullscreen').addEventListener('click', () => {
 document.addEventListener('keydown', turnOffFullscreen)
 
 document.querySelector('.filters').addEventListener('input', (e) => {
-
   const suffixCssProps = e.target.dataset.sizing || '';
   const nameCssProps = e.target.name
   const valueCssProps = e.target.value
   const outputValue = e.target.nextElementSibling
-
   document.documentElement.style.setProperty(`--${nameCssProps}`, valueCssProps + suffixCssProps)
   outputValue.value = valueCssProps
 })
 
-document.querySelector('.btn-reset').addEventListener('click', (e) => {
-  const setProps = document.documentElement.style
-  const inputs = document.querySelectorAll('.filters input')
-
+document.querySelector('.btn-reset').addEventListener('click', () => {
   inputs.forEach(input => {
     setProps.setProperty(`--${input.name}`, input.value + input.sizing)
-    input.name === 'saturate' ? input.value = 100 : input.value = 0
-  })
+    console.log(input.name === 'saturate')
 
+    input.name === 'saturate'
+      ? input.value = 100
+      : input.value = 0
+    input.name === 'saturate'
+      ? input.nextElementSibling.value = 100
+      : input.nextElementSibling.value = 0
+  })
 })
+
+document.querySelector('.editor').addEventListener('click', ({ target: { parentElement, tagName, classList } }) => {
+  if (tagName === 'BUTTON') {
+    btns.forEach(b => { b.classList.remove('btn-active') })
+    classList.add('btn-active')
+  };
+
+  if (tagName === 'INPUT') {
+    btns.forEach(b => { b.classList.remove('btn-active') })
+    parentElement.classList.add('btn-active');
+  };
+})
+
 
 
 // --------------------------------------FUNCTION
